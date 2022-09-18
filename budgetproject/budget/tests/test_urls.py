8923c88ee7bd7343a57177a225453ea275f1ccce
VinCoD <5895.2020@students.ku.ac.ke>
2022-09-18 13:02:55 +0300

    Added more url tests

--- budgetproject/budget/tests/test_urls.py
+++ budgetproject/budget/tests/test_urls.py
@@ -6,4 +6,17 @@ from budget.views import project_list, project_detail, ProjectCreateView
 class TestUrls(SimpleTestCase):
 
     def test_list_url_resolved(self):
-        url = reverse('')
+        url = reverse('list')
+
+        self.assertEqual(resolve(url).func, project_list)
+    
+    def test_add_url_resolved(self):
+        url = reverse('add')
+
+        self.assertEqual(resolve(url).func.view_class, ProjectCreateView)
+    
+    def test_detail_url_resolved(self):
+        url = reverse('detail', args=['some-slug'])
+
+        self.assertEqual(resolve(url).func, project_detail)
+        
